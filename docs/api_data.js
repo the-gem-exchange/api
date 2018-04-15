f057953b90a953b5cac11db5a1ae7d6593fc3ff1
Jon Glover <juandglover@gmail.com>
2018-04-15 10:18:11 -0700

    Create docs

---
+++ docs/api_data.js
@@ -0,0 +1,35 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/getsalt",
+    "title": "Get Salt",
+    "name": "Get_Salt",
+    "description": "<p>Get Salt to be used for password encryption.</p>",
+    "group": "Authentication",
+    "version": "0.0.0",
+    "filename": "api/routes/auth.js",
+    "groupTitle": "Authentication"
+  },
+  {
+    "type": "post",
+    "url": "/login",
+    "title": "Login",
+    "name": "Login",
+    "description": "<p>Authenticate a user with a username and password.</p>",
+    "group": "Authentication",
+    "version": "0.0.0",
+    "filename": "api/routes/auth.js",
+    "groupTitle": "Authentication"
+  },
+  {
+    "type": "post",
+    "url": "/register",
+    "title": "Register",
+    "name": "Register",
+    "description": "<p>Create a new user.</p>",
+    "group": "Authentication",
+    "version": "0.0.0",
+    "filename": "api/routes/auth.js",
+    "groupTitle": "Authentication"
+  }
+] });
